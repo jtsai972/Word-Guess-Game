87cfdffcfa40c7fd9bf327b2383130eee7c9a0b6
J Tsai <jtsai972@gmail.com>
2019-09-17 18:50:42 -0500

    setting up some basic variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+//setting up basic wordbank
+let wordBank = ["test", "exam", "javascript", "cascading", "stylesheet", "angular", "react", "node", "code"];
+
+//setting basic variables
+let countWin, countLoss, countGuess;
+
+//When key is pressed, start game
+
+//if guess is a letter then add letter to answer
+
+//else add letter to wrong
