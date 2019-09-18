a95790ed811d6ad51a9a68c7b3033eba2f0e6f6c
J Tsai <jtsai972@gmail.com>
2019-09-18 08:58:57 -0500

    just adding a random word generator

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,23 @@ let wordBank = ["test", "exam", "javascript", "cascading", "stylesheet", "angula
 //setting basic variables
 let countWin, countLoss, countGuess;
 
+//generate random word
+function random () {
+    //generate random number between 0 to wordbank size
+    let randomNum = Math.floor(Math.random() * Math.floor(wordBank.length));
+
+    console.log(randomNum);
+
+    //make it into a random word
+    let randomWord = wordBank[randomNum]; 
+
+    //return word
+    return randomWord;
+}
+console.log(random()); //checking
+
+
+
 //When key is pressed, start game
 
 //if guess is a letter then add letter to answer
