53ce928833ea3b71600e4aed2b0052b1b3002f6b
J Tsai <jtsai972@gmail.com>
2019-09-19 13:25:23 -0500

    buggy code, taking it home to clean it up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,11 @@
-//setting up basic wordbank
-    let wordBank = ["github", "element", "javascript", "cascading", "stylesheet", "angular", "react", "node", "vue", "code"];
+//setting up arrays
     let answer = [];
+    //setting up basic wordbank
+    let wordBank = ["github", "element", "javascript", "cascading", "stylesheet", "angular", "react", "node", "vue", "code"];
 
 //setting basic variables
-    let countWin, countLoss, countGuess;
+    let countWin, countLoss, countGuess; //counters
+    let correct; //boolean
 
 //generate random word
     function random () {
@@ -18,32 +20,91 @@
 
 //check for correct answer
 function checking(guess) {
+    correct = false;
+
+    //checking for match in the word
     for(let i = 0; i< countLet; i++) {
         if(guess === selWord[i]) {
+            correct = true;
             answer[i] = selWord[i];
             document.getElementById("answer").innerHTML += answer[i];
+            blanks--;
+            console.log("blank " + i + ": " + blanks);
         }
+        //need another for loop to check match on a l
     }
+    
+    //if there is no matching letter
+    if(correct !== true) {
+        countGuess--;
+        document.getElementById("wrong").innerHTML += guess;
+
+        console.log(countGuess);
+    }
+
+    if (blanks <= 0) {
+        answer = random();
+        console.log(answer);
+
+        reset();
+    }
+
+    print();
+}
+
+function printScore() {
+    document.getElementById("wins").innerHTML = countWin;
+    document.getElementById("losses").innerHTML = countLoss;
+    document.getElementById("guesses").innerHTML = countGuess;
 }
 
 function print() {
-    for(let i = 0; i< countLet; i++) {
-        document.getElementById("answer").innerHTML += answer[i];
+    document.getElementById("answer").innerHTML = answer.join("");
+}
+
+function reset() {
+    countGuess = 9;
+    selWord = random();
+    countLet = selWord.length;
+    blanks = countLet;
+
+    //Setting up blank letters
+    for (let i = 0; i < countLet; i++ ){
+        answer[i] = "_ ";
     }
+
+    printScore();
+    print();
 }
 
-let selWord = random();
-let countLet = selWord.length;
-console.log(selWord);
+    //Initiate Game
+    countLoss = countWin = 0;
+    countGuess = 9;
 
-//Setting up blank letters
-for (let i = 0; i < countLet; i++ ){
-    answer[i] = "_ ";
-}
+    var selWord = random();
+    var countLet = selWord.length;
+    var blanks = countLet;
+    console.log(selWord);
 
+    //Setting up blank letters
+    for (let i = 0; i < countLet; i++ ){
+        answer[i] = "_ ";
+    }
+
+    printScore();
+    print();
 
 
 //When key is pressed, start game
+document.onkeyup = function(event) {
+    var guess = event.key;
+
+    checking(guess);
+    
+    //updating
+    printScore();
+    print();
+}
 
 //if guess is a letter then add letter to answer
 
